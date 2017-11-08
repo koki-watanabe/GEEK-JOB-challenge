6e1268f14fbaf8dd2830401c3c505276cb452ae7
koki-watanabe <khoo.1852.1029@gmail.com>
2017-11-08 21:48:05 +0900

    課題：「Date1」の提出

---
+++ CampChallenge_java/015.標準クラス/C_kadai1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StndardClass;
+
+import java.util.Date;
+
+/**
+ *
+ * @author watanabetakashinozomi
+ */
+public class C_kadai1 {
+    public static void main (String[] args){
+        Date d = new Date(2016,1,1,0,0,0);
+        System.out.println(d.getTime());
+
+       
+        
+        
+    }
+    
+}
