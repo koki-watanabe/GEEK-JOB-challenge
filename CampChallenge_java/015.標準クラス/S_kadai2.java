02ef5814d26d013539e41a21ee742d086a12a4cb
koki-watanabe <khoo.1852.1029@gmail.com>
2017-11-09 18:57:51 +0900

    課題：「文字列2」の提出

---
+++ CampChallenge_java/015.標準クラス/S_kadai2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StndardClass.String;
+
+/**
+ *
+ * @author watanabetakashinozomi
+ */
+public class S_kadai2 {
+    public static void main (String[] args){
+        String ad = "*********@gmail.com";
+        
+        System.out.print(ad.substring(10));
+    }
+}
