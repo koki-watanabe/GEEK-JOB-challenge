07017e1681faf41f457ce7d7ec529c7f92433c59
koki-watanabe <khoo.1852.1029@gmail.com>
2017-11-09 18:58:18 +0900

    課題：「文字列3」の提出

---
+++ CampChallenge_java/015.標準クラス/S_kadai3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StndardClass.String;
+
+/**
+ *
+ * @author watanabetakashinozomi
+ */
+public class S_kadai3 {
+    public static void main(String[] args){
+        String i = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        System.out.print(i.replace("U", "う").replace("I", "い"));
+    }
+}
